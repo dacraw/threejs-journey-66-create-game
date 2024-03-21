2178bd1648e416aa6997b301311cc87415fe4814
Doug <doug.a.crawford@gmail.com>
2024-03-21 16:46:36 -0700

    Optimize by re-using geometry in Level

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,8 +1,16 @@
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
 function BlockStart({ position = [0, 0, 0] }) {
   return (
     <group position={position}>
-      <mesh position={[0, -0.1, 0]} receiveShadow>
-        <boxGeometry args={[4, 0.2, 4]} />
+      <mesh
+        position={[0, -0.1, 0]}
+        receiveShadow
+        geometry={boxGeometry}
+        scale={[4, 0.2, 4]}
+      >
         <meshStandardMaterial color="limegreen" />
       </mesh>
     </group>
