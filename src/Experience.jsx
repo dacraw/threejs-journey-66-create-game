bdda487a34673d25039157e2cac5291adf47ed24
Doug <doug.a.crawford@gmail.com>
2024-03-26 13:15:40 -0700

    Add keyboard controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,6 +3,7 @@ import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
 import { Physics } from "@react-three/rapier";
 import { Perf } from "r3f-perf";
+import Player from "./Player.jsx";
 
 export default function Experience() {
   return (
@@ -12,8 +13,8 @@ export default function Experience() {
 
       <Physics debug>
         <Lights />
-
         <Level />
+        <Player />
       </Physics>
     </>
   );
---
+++ src/Player.jsx
@@ -0,0 +1,18 @@
+import { RigidBody } from "@react-three/rapier";
+
+export default function Player() {
+  return (
+    <RigidBody
+      position={[0, 1, 0]}
+      colliders="ball"
+      restitution={0.2}
+      friction={1}
+      canSleep={false}
+    >
+      <mesh castShadow>
+        <icosahedronGeometry args={[0.3, 1]} />
+        <meshStandardMaterial flatShading color="mediumpurple" />
+      </mesh>
+    </RigidBody>
+  );
+}
