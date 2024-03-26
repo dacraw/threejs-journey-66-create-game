478193435044c72fb5b9436e2ee4abc5112f8375
Doug <doug.a.crawford@gmail.com>
2024-03-26 13:45:26 -0700

    Allow the player to jump

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,13 +1,81 @@
-import { RigidBody } from "@react-three/rapier";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { RigidBody, useRapier } from "@react-three/rapier";
+import { useEffect, useRef } from "react";
 
 export default function Player() {
+  const body = useRef();
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const { rapier, world } = useRapier();
+
+  const jump = () => {
+    const origin = body.current.translation();
+    origin.y -= 0.31;
+
+    const direction = { x: 0, y: -1, z: 0 };
+    const ray = new rapier.Ray(origin, direction);
+    const hit = world.castRay(ray, 10, true);
+
+    if (hit.toi < 0.15) {
+      body.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribeJump = subscribeKeys(
+      (state) => {
+        // selector function; want to listen to change in state of keys
+        return state.jump;
+      },
+      (value) => {
+        if (value) {
+          jump();
+        }
+      }
+    );
+
+    // cleanup to prevent multiple subscribing to jump when component changes
+    return () => {
+      unsubscribeJump();
+    };
+  });
+
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward, jump } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 0.6 * delta;
+    const torqueStrength = 0.2 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    } else if (backward) {
+      impulse.z += impulseStrength;
+      torque.x += torqueStrength;
+    } else if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    } else if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    body.current.applyImpulse(impulse);
+    body.current.applyTorqueImpulse(torque);
+  });
   return (
     <RigidBody
+      ref={body}
       position={[0, 1, 0]}
       colliders="ball"
       restitution={0.2}
       friction={1}
       canSleep={false}
+      linearDamping={0.5}
+      angularDamping={0.5}
     >
       <mesh castShadow>
         <icosahedronGeometry args={[0.3, 1]} />
