762191a3df9655e619c1b92f64913ca91217cecd
Doug <doug.a.crawford@gmail.com>
2024-03-26 17:04:05 -0700

    Add interface

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
 import { KeyboardControls } from "@react-three/drei";
+import Interface from "./Interface.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -16,6 +17,7 @@ root.render(
       { name: "jump", keys: ["Space"] },
     ]}
   >
+    <Interface />
     <Canvas
       shadows
       camera={{
